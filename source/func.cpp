90e599cf142c0dc4e8a6c0763350a899f2603dc4
Florian Hagemann <florianhagemann@mail.de>
2025-10-26 20:12:48 +0100

    cleaned repo of test code

--- source/func.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include "func.h"
-
-using namespace std;
-
-int func(int a, int b) {
-    cout << a + b << endl;
-    return a + b;
-}
--- source/main.cpp
+++ source/main.cpp
@@ -1,11 +1,10 @@
 #include<iostream>
-#include "func.h"
 
 using namespace std;
 
 int main() {
     
-    cout << func(1,2) << endl;
+
     return 0;
 
 }
