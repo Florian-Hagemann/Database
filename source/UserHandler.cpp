1faaffd19733e3e1ca0632cf2b62f5e51268d779
Florian Hagemann <florianhagemann@mail.de>
2025-10-27 18:49:45 +0100

    working now OwO

--- source/UserHandler.cpp
+++ source/UserHandler.cpp
@@ -9,25 +9,53 @@ void UserHandler::parseInput() {
     // expected is input in form of [command] [arg1] [arg2] ...
     // number of args ofc depends on command
 
-    int delimiterIndex = -1;
-    int count = 0;
+    int lastIndex = 0;
+    int nextIndex = 0;
+    int subStringCounter = 0;
+    bool endReached = false;
 
-    while(rawInput.find(' ', delimiterIndex + 1) != std::string::npos) {
+    while(!endReached) {
 
-        delimiterIndex = rawInput.find(' ', delimiterIndex + 1);
+        std::cout << "Substring counter: " << subStringCounter << std::endl;
 
-        // catching out of bounds exception
-        try {
+        nextIndex = rawInput.find(' ', lastIndex + 1);
+        endReached = (rawInput.find(' ', lastIndex) == -1) ? true : false;
+        
+        std::cout << "End reached? : " << endReached << std::endl;
+        std::cout << "Next reached index: " << nextIndex << std::endl;
 
-            parsedInput[count] = rawInput.substr(delimiterIndex, rawInput.find(' ', delimiterIndex + 1));
+        if(endReached) {
+            /*
+            std::cout << "End reached index: " << lastIndex << std::endl;
+            
+            try {
+                
+                parsedInput.at(subStringCounter) = rawInput.substr(lastIndex, rawInput.length() - 1);
 
-        } catch(const std::out_of_range& e) {
+            } catch(const std::out_of_range& e) {
 
-            parsedInput.push_back(rawInput.substr(delimiterIndex, rawInput.find(' ', delimiterIndex + 1)));
+                parsedInput.push_back(rawInput.substr(lastIndex, rawInput.length() - 1));
+
+            }
+*/
+        } else {
+
+            try {
+
+                parsedInput.at(subStringCounter) = rawInput.substr(lastIndex, nextIndex - lastIndex);
+
+            } catch(const std::out_of_range& e) {
+
+                parsedInput.push_back(rawInput.substr(lastIndex, nextIndex - lastIndex));
+
+            }
+
+            std::cout << parsedInput.at(subStringCounter) << std::endl; 
 
         }
 
-        count++;
+        lastIndex = nextIndex;
+        subStringCounter++;
 
     }
 
@@ -36,14 +64,14 @@ void UserHandler::parseInput() {
 int UserHandler::handleInput() {
 
     std::cout << "$ db ";
-    std::cin >> rawInput;
+    std::getline(std::cin, rawInput);
 
     std::cout << rawInput << std::endl;
 
     parseInput();
 
-    for(std::string s : parsedInput) {
-        std::cout << s << std::endl;
+    for(int i = 0; i < parsedInput.size(); i++) {
+        std::cout << parsedInput.at(i) << std::endl;
     }
 
     return 1;
