0f5b94d9dc80872401d56c0e22d0bde6b0333db9
Florian Hagemann <florianhagemann@mail.de>
2025-10-28 23:51:06 +0100

    add test case

--- tests/Test_UserHandler.cpp
+++ tests/Test_UserHandler.cpp
@@ -7,12 +7,11 @@ TEST(TestUserHandler, TestParsing) {
     UserHandler userHandler;
 
     // test cases
-    std::vector<std::string> testCase = {"123 abc 321", "aaaaaaaaaa", "hello world"};
-    std::vector<std::vector<std::string>> expectedResult = {{"123", "abc", "321"}, {"aaaaaaaaaa"}, {"hello", "world"}};
+    std::vector<std::string> testCase = {"123 abc 321", "aaaaaaaaaa", "hello world", "heyyy  yooo"};
+    std::vector<std::vector<std::string>> expectedResult = {{"123", "abc", "321"}, {"aaaaaaaaaa"}, {"hello", "world"}, {"heyyy", "yoo"}};
 
     // test those cases
-    // ! make sure that there are as many test cases as expected results !
-    // ! else test will FAIL !
+    // ! make sure that there are as many test cases as expected results
     // TODO make sure that doesnt happen
     for(int i = 0; i < testCase.size(); i++) {
         EXPECT_EQ(userHandler.parseInput(&testCase.at(i)), expectedResult.at(i));
