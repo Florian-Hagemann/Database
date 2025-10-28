bfceccf0b214bfbb3f29dfd5a5fea2b591c343e7
Florian Hagemann <florianhagemann@mail.de>
2025-10-29 00:05:56 +0100

    removed testing args

--- tests/Test_UserHandler.cpp
+++ tests/Test_UserHandler.cpp
@@ -8,7 +8,7 @@ TEST(TestUserHandler, TestParsing) {
     UserHandler userHandler;
 
     // test cases
-    std::vector<std::string> testCase = {"123 abc 321", "aaaaaaaaaa", "hello world", "heyyy  yooo", "a"};
+    std::vector<std::string> testCase = {"123 abc 321", "aaaaaaaaaa", "hello world", "heyyy  yooo"};
     std::vector<std::vector<std::string>> expectedResult = {{"123", "abc", "321"}, {"aaaaaaaaaa"}, {"hello", "world"}, {"heyyy", "yooo"}};
 
     // test those cases
