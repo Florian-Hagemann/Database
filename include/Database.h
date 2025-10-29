94e0e6c1a2d1f96ba9e82ad3b3765338d5f16251
Florian Hagemann <florianhagemann@mail.de>
2025-10-29 21:08:45 +0100

    coded opening the file

--- include/Database.h
+++ include/Database.h
@@ -1,8 +1,11 @@
 #include<string>
+#include<fstream>
 
 class Database{
-
+    std::fstream fileStream;
     public:
+    void open(std::string fileName);
+    void close(std::string fileName);
     void append();
     void remove();
 };
--- source/Database.cpp
+++ source/Database.cpp
@@ -1,3 +1,26 @@
 #include"Database.h"
 #include<string>
+#include<fstream>
 
+void Database::open(std::string fileName) {
+
+    // Close open file stream
+    if(fileStream.is_open()) {
+        fileStream.close();
+    }
+
+    // open new one
+    fileStream.open(fileName + ".db");
+
+    // check if it worked
+    if(!fileStream.is_open()) {
+        throw std::runtime_error("Failed to open database file: " + fileName + ".db");
+    }
+
+}
+
+void Database::close(std::string fileName) {
+
+    fileStream.close();
+
+}
